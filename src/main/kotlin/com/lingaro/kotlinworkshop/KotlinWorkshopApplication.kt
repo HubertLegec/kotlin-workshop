772b12c15d3c256eed72608a04dbfbd0e6751e17
HubertLegec <hubert.legec@gmail.com>
2017-12-17 18:25:50 +0100

    initial commit

---
+++ src/main/kotlin/com/lingaro/kotlinworkshop/KotlinWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package com.lingaro.kotlinworkshop
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinWorkshopApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinWorkshopApplication::class.java, *args)
+}
